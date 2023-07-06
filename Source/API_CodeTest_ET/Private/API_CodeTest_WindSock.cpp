27c5e2a76dcfb2f625f6c9ba84dc1e059809ec2f
SAETAK <ihjang404@gmail.com>
2023-07-06 09:53:35 +0900

    add Body Component & Static Component

--- Source/API_CodeTest_ET/Private/API_CodeTest_WindSock.cpp
+++ Source/API_CodeTest_ET/Private/API_CodeTest_WindSock.cpp
@@ -2,13 +2,20 @@
 
 
 #include "API_CodeTest_WindSock.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AAPI_CodeTest_WindSock::AAPI_CodeTest_WindSock()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = Box;
 
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	Body->SetupAttachment(Body);
 }
 
 // Called when the game starts or when spawned
